de1d012c0a8f46040a578e27d82787b27b0d2347
Michael Zornek <mike@clickablebliss.com>
2015-03-13 23:04:17 -0400

    Add proper setting of the header behavior.

--- handlers.go
+++ handlers.go
@@ -17,6 +17,8 @@ func TodoIndex(w http.ResponseWriter, r *http.Request) {
 		Todo{Name: "Write presentation"},
 		Todo{Name: "Host meetup"},
 	}
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 	if err := json.NewEncoder(w).Encode(todos); err != nil {
 		panic(err)
 	}
